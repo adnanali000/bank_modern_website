cf3e7274861652575bf18a08075b50d2187189fe
adnanali000 <adnan.museel123@gmail.com>
2022-08-21 01:14:52 +0500

    CTA work done

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -6,7 +6,7 @@ const Clients = () => (
       <div className={`${styles.flexCenter} flex-wrap w-full`}>
         {clients.map((client,index)=>(
           <div key={client.id} className={`flex-1 brands ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
-            <img src={client.logo} alt="client" className={`sm:w-[192px] w-[100px] object-contain`} />
+            <img src={client.logo} alt="client" className={`sm:w-[192px] w-[100px] object-contain mb-2`} />
           </div>
         ))}
 
