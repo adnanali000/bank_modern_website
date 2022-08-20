c4d9a37bd85213fc899be1e692317a2ce712d2f8
adnanali000 <adnan.museel123@gmail.com>
2022-08-20 12:47:37 +0500

    navbar and folder structure work done

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from './Navbar';
+import Button from './Button';
+import CTA from './CTA';
+import CardDeal from './CardDeal';
+import Clients from './Clients';
+import Feedback from './Feedback';
+import Billing from './Billing';
+import Footer from './Footer';
+import Stats from './Stats';
+import Hero from './Hero';
+import Testimonials from './Testimonials';
+import GetStarted from './GetStarted';
+import Business from './Business'
+
+export {
+    Navbar,
+    Button,
+    CTA,
+    GetStarted,
+    CardDeal,
+    Clients,
+    Feedback,
+    Billing,
+    Footer,
+    Stats,
+    Hero,
+    Testimonials,
+    Business
+}
